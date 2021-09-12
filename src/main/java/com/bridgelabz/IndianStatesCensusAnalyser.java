32d057f0f5d2c735685a00293df4fbbd7be99c7b
Gaurip10 <thegauriparhad@gmail.com>
2021-09-12 19:51:40 +0530

    Added: Welcome to IndianStatesCensusAnalyser

---
+++ src/main/java/com/bridgelabz/IndianStatesCensusAnalyser.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+import com.opencsv.*;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class IndianStatesCensusAnalyser {
+    public static void main(String[] args){
+        System.out.println("Welocme to Indian states census analyser");
+        String csvfile="CensusAnalyser.csv";
+        CSVReader csvReader=null;
+
+    }
+}
