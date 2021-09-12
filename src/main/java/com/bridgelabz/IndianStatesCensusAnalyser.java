9c6bce5379295642922d295a01a8b1fd3eaca6b1
Gaurip10 <thegauriparhad@gmail.com>
2021-09-12 20:30:22 +0530

    Added: Indianstates census analyser csv file

--- src/main/java/com/bridgelabz/IndianStatesCensusAnalyser.java
+++ src/main/java/com/bridgelabz/IndianStatesCensusAnalyser.java
@@ -6,8 +6,16 @@ import java.io.IOException;
 public class IndianStatesCensusAnalyser {
     public static void main(String[] args){
         System.out.println("Welocme to Indian states census analyser");
-        String csvfile="CensusAnalyser.csv";
+        String csvFile="CensusAnalyser.csv";
         CSVReader csvReader=null;
-
+        try{
+            csvReader=new CSVReader(new FileReader(csvFile));
+            String[] value;
+            while((value= csvReader.readNext() !=null){
+                System.out.println(value[0]+ " "+ value[1]+ " "+ value[2]+ " "+ value[3]+ " "+ value[4]+ " "+ value[5]+ " "+ value[6]+ " "+ value[7]);
+            }
+        } catch(IOException e){
+            System.out.println(e);
+        }
     }
 }
--- src/test/java/com/bridgelabz/IndianStatesCensusAnalyser_SadTest.java
+++ src/test/java/com/bridgelabz/IndianStatesCensusAnalyser_SadTest.java
@@ -1,7 +1,28 @@
 package com.bridgelabz;
+import java.io.FileReader;
+import java.io.UncheckedIOException;
+
+import com.opencsv.CSVReader;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class IndianStatesCensusAnalyser_SadTest {
+    String csvFile = "CensusAnalyser.csv";
+    CSVReader csvReader = null;
 
+    @Test
+    public void toCheck_Match_CSVData_Sadtest() {
+        try {
+            String csvFile = "CensusAnalyser.csv";
+            CSVReader csvReader = null;
+            String[] value;
+            while ((value = csvReader.readNext()) == null) {
+                System.out.println("Error Occurred");
+
+            }
+        } catch (UncheckedIOException e) {
+            System.out.println("Data are not matched");
+            System.out.println("Sad Test" + e);
+        }
+    }
 }
